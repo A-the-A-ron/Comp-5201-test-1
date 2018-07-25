293083697e1112653796d007835d90b9dd495cfd
Aaron Hansen <30009474@student.toiohomai.ac.nz>
2018-07-25 16:41:14 +1200

    alpha 1

---
+++ App1/App1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //is there a way to make this type out like a scifi-robot
+            Console.WriteLine("hello world");
+            //Console.ReadLine();
+        }
+    }
+}
